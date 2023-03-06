c129aa86145d2d5b82f098877ff08be34c953fe1
Charulata Mhaiskey <109023984+charulatamhaiskey@users.noreply.github.com>
2023-03-06 21:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 //complete this code
-class Person {}
+class Person {
+	private String name;
+	private int age;
 
-class Student extends Person {}
+	public Person(String name,int age){
+		this.name=name;
+		this.age=age;
+	}
 
-class Teacher extends Person {}
+	public String getName(){
+		return name;
+	}
+	public int getAge(){
+		return age;
+	}
+
+	public void setName(String name){
+		this.name=name;
+	}
+
+	public void setAge(int age){
+		this.age=age;
+	}
+}
+
+class Student extends Person 
+	
+
+	public void study(String name){
+		Student student=new Student();
+		String name=student.setName(name);
+		System.out.println(name+""+"is studying");
+}
+}
+
+class Teacher extends Person {
+	public void teach(String name){
+		Teacher t=new Teacher();
+		String name=t.setName(name);
+		System.out.println(name+""+"is teaching");
+	}
+}
+}
 
 // Do not change the code below this line
 window.Person = Person;
